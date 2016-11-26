6da7c1c3aa632317475910402f28e34c1b3494b2
karan <wahiekaran@gmail.com>
2016-11-27 03:16:49 +0530

    frontend

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var fuckApp = angular.module('fuckApp', ['ngRoute', 'ngResource', 'ngAnimate', 'ngAria', 'ngMaterial']);
 
 fuckApp.config(function ($routeProvider) {
-  
+
   $routeProvider
   .when('/', {
     templateUrl: 'html/base.html'
   })
   .when('/home', {
-    templateUrl: 'html/home.html',
+    templateUrl: 'html/temp.html',
     controller: 'homeController'
   });
-  
+
 });
--- homeController.js
+++ homeController.js
@@ -2,4 +2,5 @@ fuckApp.controller('homeController',
   function ($scope, $mdDialog, $http, baseService) {
     console.log(baseService.loginData);
     $scope.firstName = baseService.loginData.user_first_name;
+    console.log($scope);
   });
